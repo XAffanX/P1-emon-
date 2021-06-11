de38948e8d3bfa505fa68de7d9c7884797ee6ac4
XAffanX <76178858+XAffanX@users.noreply.github.com>
2021-06-12 01:21:56 +0600

    Update p1(emon).py

--- p1(emon).py
+++ p1(emon).py
@@ -1,11 +1,11 @@
-x = []
-y = ['40', '60', '3600']
-z = 0
-print("1)What is the base damage of vandal?\n2)How much does sage heal?\n3)What is the price of odin?")
-for i in range(0, 3):
-    a = input(f'{i+1})')
-    x.append(a)
+q = ["1)What is the base damage of vandal?",
+     "2)How much does sage heal?", "3)What is the price of odin?"]
+a, l, z = ['40', '60', '3600'], [], 0
+for i in q:
+    print(i)
+    v = input()
+    l.append(v)
 for i in range(3):
-    if x[i] == y[i]:
+    if a[i] == l[i]:
         z = z+1
-print('Total Points:', z)
+print("total points:", z)
